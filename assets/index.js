c5fda76ab04d22181e7f289af5a8ababdb97ef8e
Deadcoder <139893905+Deadpoolhp-4@users.noreply.github.com>
2024-06-07 20:26:46 +0530

    css

--- assets/index.js
+++ assets/index.js
@@ -2,3 +2,30 @@ function flipCard(cardId) {
     var card = document.getElementById(cardId);
     card.style.transform = card.style.transform === 'rotateY(180deg)' ? 'rotateY(0deg)' : 'rotateY(180deg)';
 }
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 10
+        },
+        480: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 30
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 40
+        }
+    }
+});
